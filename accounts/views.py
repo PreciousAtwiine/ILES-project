ddfcf55c47543ea67b829a9760cb86ba3b2bb47b
hiltonjolly-2020 <hiltonjolly@gmail.com>
2026-03-05 14:14:40 -0800

    Refactor
    Removed unused imports and cleaned up the signup view.

--- accounts/views.py
+++ accounts/views.py
@@ -1,20 +1,2 @@
-from django.shortcuts import render,redirect
-from .form import signupForm
-from .models import student
-# Create your views here.
-def signup(request):
-    if request.method == 'POST':
-        details =signupForm(request.POST)
-        if details.is_valid():
-            user= details.save()
-            student_no=request.POST['student_no']
-            student.objects.create(
-            user = user,
-            student_no =student_no
-            )
-            
-            return redirect('/reason/')
-    else:
-        details = signupForm()
-    return render(request,'signup.html',{'details': details})    
+from django.shortcuts import render
         
