691ad7cbdf5be45491316d798a871ffd88d8d571
hiltonjolly-2020 <hiltonjolly@gmail.com>
2026-03-05 14:23:36 -0800

    Delete accounts/form.py

--- accounts/form.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import User
-from rest_framework import serializers
-class signupSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=User
-        fields=['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
