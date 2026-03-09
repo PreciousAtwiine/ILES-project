604f6e38f7b54b5225b5d48478341c8e7437936f
hiltonjolly-2020 <hiltonjolly@gmail.com>
2026-03-09 11:42:12 -0700

    Update url.py

--- ILES_app/url.py
+++ ILES_app/url.py
@@ -5,10 +5,9 @@ from .views import *
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'placements', InternshipPlacementViewSet)
-router.register(r'logs', WeeklyLogViewSet)
-router.register(r'evaluations', EvaluationViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
     path('api/register/', RegisterView.as_view(), name='register'),
 ]
+
