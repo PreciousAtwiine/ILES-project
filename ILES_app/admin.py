4ed9e143ae8af4dd1623298bd393625af24ecd12
hiltonjolly-2020 <hiltonjolly@gmail.com>
2026-03-09 11:42:57 -0700

    Update admin.py

--- ILES_app/admin.py
+++ ILES_app/admin.py
@@ -3,5 +3,3 @@ from .models import User,InternshipPlacement,WeeklyLog,Evaluation
 # Register your models here.
 admin.site.register(User)
 admin.site.register(InternshipPlacement)
-admin.site.register(WeeklyLog)
-admin.site.register(Evaluation)
