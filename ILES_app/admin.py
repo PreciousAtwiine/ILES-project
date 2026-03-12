29f3d28ea6b7be49a407e48fef597fbf66b83617
hiltonjolly-2020 <hiltonjolly@gmail.com>
2026-03-12 12:07:00 -0700

    Register WeeklyLog and Evaluation models in admin

--- ILES_app/admin.py
+++ ILES_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import User,InternshipPlacement
+from .models import User,InternshipPlacement,WeeklyLog
 # Register your models here.
 admin.site.register(User)
 admin.site.register(InternshipPlacement)
 
+admin.site.register(WeeklyLog)
+admin.site.register(Evaluation)
