a83ce8f156ae7794cc630207c8fe7db974a263d7
joelkabanda <joelkabanda45@gmail.com>
2026-04-28 09:52:03 +0300

    Added workplace supervisor pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,14 @@ import Register from "./Register";
 import StudentDashboard from "./Pages/StudentDashboard";
 import WorkplaceSupervisorDashboard from "./Pages/WorkplaceSupervisorDashboard";
 import AcademicDashboard from "./Pages/AcademicDashboard";
-import PendingLogs from "./Pages/PendingLogs";
+
+import StudentsPage from "./Pages/StudentsPage";
+import PendingLogsPage from "./Pages/PendingLogsPage";
+import ReviewLogPage from "./Pages/ReviewLogPage";
+import EvaluationPage from "./Pages/EvaluationPage";
+
+
+
 function App() {
   return (
     <Router>
@@ -22,8 +29,14 @@ function App() {
         <Route path="/student" element={<StudentDashboard />} />
         <Route path="/workplace-supervisor" element={<WorkplaceSupervisorDashboard />} />
         <Route path="/supervisor" element={<AcademicDashboard />} />
-        <Route path="/pending-logs" element={<PendingLogs />} />
+        <Route path="/supervisor/dashboard" element={<WorkplaceSupervisorDashboard />} />
+        <Route path="/supervisor/students" element={<StudentsPage />} />
+        <Route path="/supervisor/pending-logs" element={<PendingLogsPage />} />
+        <Route path="/supervisor/review/:id" element={<ReviewLogPage />} />
+        <Route path="/supervisor/evaluations" element={<EvaluationPage />} />
+        
         <Route path="/workplace-supervisor-dashboard" element={<WorkplaceSupervisorDashboard />} />
+
       </Routes>
     </Router>
   );
