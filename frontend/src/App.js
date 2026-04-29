ebf8c7eac76001599e809d8741d7b4e1a3443d89
Mervyn Marvyn <113608285+mervyn-iconic@users.noreply.github.com>
2026-04-29 20:01:31 +0300

    Added Admin Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,7 +24,7 @@ function App() {
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-
+        <Route path="/admin" element={<AdminDashboard />} />
         {/* Student */}
         <Route path="/student" element={<StudentDashboard />} />
 
