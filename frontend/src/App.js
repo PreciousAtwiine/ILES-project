ddf8768ac2885a372e46d8545e684009bc3580ae
joelkabanda <joelkabanda45@gmail.com>
2026-05-03 14:27:20 +0300

    Added combined dshboard file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./Login";
 import Register from "./Register";
-import StudentDashboard from "./Pages/StudentDashboard";
-
-import SupervisorDashboard from "./Pages/SupervisorDashboard";
-import AcademicEvaluation from "./Pages/AcademicEvaluation";
-
-
-
-
-import AcademicDashboard from "./Pages/AcademicDashboard";
-
-import StudentsPage from "./Pages/StudentsPage";
-import PendingLogsPage from "./Pages/PendingLogsPage";
-import ReviewLogPage from "./Pages/ReviewLogPage";
-import EvaluationPage from "./Pages/EvaluationPage";
-import AdminDashboard from "./Pages/AdminDashboard";
-
+import Dashboard from "./Pages/Dashboard";  // ← Fixed: ./Pages/Dashboard (capital P)
 
 function App() {
   return (
@@ -25,20 +10,8 @@ function App() {
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-
-        {/* Student */}
-        <Route path="/student" element={<StudentDashboard />} />
-
-        {/* Academic Supervisor*/}
-        <Route path="/academic" element={<AcademicDashboard />} />
-  
-        <Route path="/academic/evaluate" element={<AcademicEvaluation/>} />
-
-        {/* Workplace Supervisor */}
-         <Route path="/dashboard" element={<SupervisorDashboard />} />
-        
-        
-
+        <Route path="/dashboard" element={<Dashboard />} />
+		
       </Routes>
     </Router>
   );
