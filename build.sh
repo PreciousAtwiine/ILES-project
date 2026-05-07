b014d43d502edd3ae96cdd1345d0846b488f675c
Atwiine Precious <preciousinshuti3@gmail.com>
2026-05-08 01:42:24 +0300

    Superuser

--- build.sh
+++ build.sh
@@ -12,12 +12,12 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 #Creating superuser
-python manage.py shell -c "
+python manage.py shell -c '
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser('admin','admin@iles.com','admin123')
-    print('Superuser created')
+    User.objects.create_superuser("admin", "admin@iles.com", "admin123")
+    print("Superuser created")
 else:
-    print('Superuser already exists')
-"
+    print("Superuser already exists")
+'
