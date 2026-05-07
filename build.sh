b46e5e59e48fe60e7ab410035576f96a44bebfe4
Atwiine Precious <preciousinshuti3@gmail.com>
2026-05-08 01:36:31 +0300

    Created superuser in build.sh

--- build.sh
+++ build.sh
@@ -10,3 +10,14 @@ python manage.py collectstatic --no-input
 
 # Run database migrations
 python manage.py migrate
+
+#Creating superuser
+pyhton manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser('admin','admin@iles.com','admin123')
+    print('Superuser created')
+else:
+    print('Superuser already exists')
+"
