7ef80dbae98946e17874707009c9a480e6a9814f
Atwiine Precious <preciousinshuti3@gmail.com>
2026-05-08 00:12:58 +0300

    Settings

--- ILES/settings.py
+++ ILES/settings.py
@@ -136,9 +136,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/dist/assets'),
-]
+#Not needed on render
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'frontend/dist/assets'),
+# ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AUTH_USER_MODEL='ILES_app.User'
