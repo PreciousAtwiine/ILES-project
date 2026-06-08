33a824d90113931546c3785f5667dee9405bcc00
hiltonjolly-2020 <hiltonjolly@gmail.com>
2026-06-07 23:10:46 -0700

    Update settings.py

--- ILES/settings.py
+++ ILES/settings.py
@@ -209,7 +209,7 @@ CORS_ALLOW_HEADERS = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'https://iles-project-group6.onrender.com',  
+    'https://iles-project-g6.onrender.com' 
     'http://localhost:3000',
     'http://127.0.0.1:3000',
 ]
@@ -218,5 +218,5 @@ CSRF_TRUSTED_ORIGINS = [
     'https://*.onrender.com',
     'https://*.vercel.app',
     'http://localhost:3000',
-    'https://iles-project-group6.onrender.com',  
+    'https://iles-project-g6.onrender.com',  
 ]
