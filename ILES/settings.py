0fa54ec0071b1c8429602694085d78bb8f9cbf55
hiltonjolly-2020 <hiltonjolly@gmail.com>
2026-06-08 02:34:47 -0700

    email sys

--- ILES/settings.py
+++ ILES/settings.py
@@ -159,17 +159,17 @@ LOGIN_REDIRECT_URL = '/api/student/dashboard/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+# Email settings
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
-    EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
-    EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-    EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
-    EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-    DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='ILES System <noreply@iles.com>')
-
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_USER = 'hiltonjolly@gmail.com'
+    EMAIL_HOST_PASSWORD = 'tcufomcfcxeinclj' 
+    DEFAULT_FROM_EMAIL = 'ILES System <'hiltonjolly@gmail.com>'
 FRONTEND_URL = config('FRONTEND_URL', default='https://iles-project-group6.onrender.com')
 
 
