fd386bb8d2dbcff66b537aaa97add6968a80bc48
Atwiine Precious <preciousinshuti3@gmail.com>
2026-06-10 12:25:23 +0300

    Emmail settings

--- ILES/settings.py
+++ ILES/settings.py
@@ -166,13 +166,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_PORT = 587
-    EMAIL_USE_TLS = True
-    EMAIL_HOST_USER = 'hiltonjolly@gmail.com'
-    EMAIL_HOST_PASSWORD = 'tcufomcfcxeinclj' 
-    DEFAULT_FROM_EMAIL = 'ILES System <hiltonjolly@gmail.com>'
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  # fallback, won't be used
+
+RESEND_API_KEY = config('RESEND_API_KEY', default='')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='noreply@yourdomain.com')
 FRONTEND_URL = config('FRONTEND_URL', default='https://iles-project-group6.onrender.com')
 
 
