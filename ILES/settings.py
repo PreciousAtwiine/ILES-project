923f8658a99feedc97e43d474af6ac057d3b0fb2
Atwiine Precious <preciousinshuti3@gmail.com>
2026-06-10 16:04:06 +0300

    Common validator

--- ILES/settings.py
+++ ILES/settings.py
@@ -88,8 +88,8 @@ else:
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', 'OPTIONS': {'max_similarity': 0.55}},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', 'OPTIONS': {'min_length': 8}},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+    # {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
 # Internationalization
