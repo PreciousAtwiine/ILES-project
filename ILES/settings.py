4fc223a44ad1904131ee3f29d672d959d4fdf312
hiltonjolly-2020 <hiltonjolly@gmail.com>
2026-06-10 06:57:15 -0700

    Modify settings.py for Brevo email integration
    Updated Django settings to include Anymail for Brevo integration and adjusted email backend configurations.

--- ILES/settings.py
+++ ILES/settings.py
@@ -128,16 +128,15 @@ MEDIA_ROOT = BASE_DIR / 'media'
 FRONTEND_URL = config('FRONTEND_URL', default='https://iles-project-group6.onrender.com')
 
 
+
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = 'smtp-relay.brevo.com'
-    EMAIL_PORT = 587
-    EMAIL_USE_TLS = True
-    EMAIL_HOST_USER = os.environ.get('BREVO_SMTP_USER', 'hiltonjolly@gmail.com')
-    EMAIL_HOST_PASSWORD = os.environ.get('BREVO_SMTP_PASSWORD')
-    DEFAULT_FROM_EMAIL = 'hiltonjolly@gmail.com'  
+    EMAIL_BACKEND = "anymail.backends.brevo.EmailBackend"
+    ANYMAIL = {
+        "BREVO_API_KEY": os.environ.get("BREVO_API_KEY"),
+    }
+    DEFAULT_FROM_EMAIL = 'hiltonjolly@gmail.com'   
 
 # CORS
 CORS_ALLOWED_ORIGINS = [
